dfead01a9399e763b7bb16591f3d8c05a9e3f633
Chyvikov-Anton <chivikovanton@gmail.com>
2020-12-17 17:44:55 +0200

    Added solution acc. OOP exercise

--- index.js
+++ index.js
@@ -7,72 +7,84 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-const dog = {
-   species: 'dog',
-   name: 'Spike',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'Woof-Woof!',
-   friends: ['John'],
+
+class Inhabitant {
+    constructor(name, gender, friends) {
+        //At the moment I can't imagine what the creatures will inhabit our world, that is why I assumed that these will be some people and different kinds of their pets. In this way I can assume that they all would have these common properties and one method to perform the task.
+        this.name = name;
+        this.gender = gender;
+        this.friends = friends;
+        this.templete = ` My name is ${this.name}. My gender is ${this.gender}.`;
+    };
+
+    getVoice() {
+        let pluralEnding;
+        (this.friends && this.friends.length > 1) ? pluralEnding = 's': pluralEnding = '';
+        return (this.friends) ? this.templete + `I have friend${pluralEnding}: ${this.friends.join(', ')}.` : this.templete + ' I am looking for friends.';
+    };
 };
 
-const cat = {
-   species: 'cat',
-   name: 'Tom',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'Meow-Meow!',
-   friends: ['John'],
-};
+class Animal extends Inhabitant {
+    constructor(...args) {
+        super(...args);
+        this.legs = 4;
+    };
 
-const woman = {
-   species: 'human',
-   name: 'Jane',
-   gender: 'female',
-   legs: 2,
-   hands: 2,
-   saying: 'Nice to meet you!',
-};
+    getVoice() {
+        return super.getVoice() + ` I have ${this.legs} legs.`;
+    };
+}
 
-const man = {
-   friends: ['Spike', 'Tom'],
-   species: 'human',
-   name: 'John',
-   legs: 2,
-   hands: 2,
-   saying: 'Hi, how are you?',
-   gender: 'male',
-};
+class Human extends Inhabitant {
+    constructor(saying, ...args) {
+        super(...args);
+        //I assumed that people can say different phrases
+        this.saying = saying;
+        this.species = 'human';
+        this.hands = 2;
+        this.legs = 2;
+    };
 
-function createGreetingContent(obj) {
+    getVoice() {
+        return `I am a ${this.species}. ` + super.getVoice() + ` I have ${this.legs} legs and ${this.hands} hands. I want to say you: "${this.saying}".`;
+    };
+}
 
-   const template = {
-      species: 'I am a %',
-      name: 'My name is %',
-      gender: 'My gender is %',
-      hands: 'I have % hands',
-      legs: 'I have % legs',
-      saying: 'I want to say you: "%"',
-      friends: 'I have friend %',
-   };
+class Dog extends Animal {
+    constructor(...args) {
+        super(...args);
+        this.species = 'dog';
+        this.saying = 'Woof-Woof!'; //I assumed that all dogs say the same
+    };
 
-   const greeting = Object.keys(template).map((key) => {
-      if (obj[key] && template[key]) {
-         return template[key].replace('%', obj[key]);
-      };
-   }).filter(item => item != undefined).join('. ');
+    getVoice() {
+        return `I am a ${this.species}. ` + super.getVoice() + ` I want to say you: "${this.saying}".`;
+    };
+}
 
-   return `${greeting}.`;
-};
+class Cat extends Animal {
+    constructor(...args) {
+        super(...args);
+        this.species = 'cat';
+        this.saying = 'Meow-Meow!'; //I assumed that all cats say the same
+    };
 
-const inhabitants = [dog, cat, woman, man,];
+    getVoice() {
+        return `I am a ${this.species}. ` + super.getVoice() + ` I want to say you: "${this.saying}".`;
+    };
+}
+
+const dog = new Dog('Spike', 'male', ['John']);
+const cat = new Cat('Tom', 'male', ['John']);
+const woman = new Human('Nice to meet you!', 'Jane', 'female');
+const man = new Human('Hi, how are you?', 'John', 'male', ['Spike', 'Tom']);
+
+const inhabitants = [dog, cat, woman, man];
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
- 
+
    Message can contain HTML markup. You may also tweak index.html and/or styles.css.
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.
@@ -82,12 +94,12 @@ const inhabitants = [dog, cat, woman, man,];
    print('ABC');
    print('<strong>ABC</strong>');
    print('<strong>ABC</strong>', 'div');
- 
+
    print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
 
-inhabitants.forEach((item) => {
-   print(createGreetingContent(item), 'div');
+inhabitants.forEach((inhabitant) => {
+    print(inhabitant.getVoice(), 'div');
 });
